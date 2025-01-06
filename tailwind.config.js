ccad2c54a9c77b099877604e3ec3b50221833d3d
patau23 <akejan_2302@mail.ru>
2025-01-07 01:57:31 +0500

    commit some package changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'c-blue': '#62BAF3'
+      },
+      fontFamily: {
+        sans: ['Rubik', 'Arial', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ matchComponents, theme }) => {
+      matchComponents(
+        {
+          "truncate": (value) => ({
+            display: "-webkit-box !important",
+            "-webkit-box-orient": "vertical",
+            "-webkit-line-clamp": value, /* Number of lines to show */
+            overflow: "hidden",
+          })
+        },
+        {
+          values: theme("truncate"),
+        }
+      );
+    }
+  ],
 };
